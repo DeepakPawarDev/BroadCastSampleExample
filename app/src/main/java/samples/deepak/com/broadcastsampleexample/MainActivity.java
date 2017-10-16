9d9c832d490aa85c229195c6cf25330285e1865b
deepak <pawardeepakv@gmail>
2017-10-16 16:18:35 +0530

    Edited By Deepak

--- app/src/main/java/samples/deepak/com/broadcastsampleexample/MainActivity.java
+++ app/src/main/java/samples/deepak/com/broadcastsampleexample/MainActivity.java
@@ -1,17 +1,37 @@
 package samples.deepak.com.broadcastsampleexample;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "Send", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.setAction("com.my.broadcast");
+                sendBroadcast(intent);
+
+
+            }
+        });
 
 
     }
